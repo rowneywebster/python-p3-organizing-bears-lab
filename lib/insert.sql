71efd123aab866835ee76d8dea185de591d2f5fe
Rowney Ouma <oumarowney@gmail.com>
2025-07-20 14:00:03 +0300

    All done

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Calm');
+
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Rowdy', 7, 'M', 'Black', 'Aggressive');
+
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Tabitha', 4, 'F', 'Cinnamon', 'Playful');
+
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Sergeant Brown', 6, 'M', 'Brown', 'Serious');
+
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Melissa', 3, 'F', 'White', 'Gentle');
+
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Grinch', 8, 'M', 'Grey', 'Grumpy');
+
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Wendy', 2, 'F', 'Blonde', 'Curious');
+
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES (NULL, 4, 'F', 'Black', 'Shy');
